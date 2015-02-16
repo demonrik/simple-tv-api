b6c6f774a37d7cf1b32cd11b00dfc0a1cff605e5
demonrik <rik.dunphy@gmail.com>
2015-02-16 14:03:15 -0700

    Updated the quality selection to be controled from the download.py as a string with the bitrate.. URL selected from the STV contains tv.bitrate.ID  so string match to select

--- download.py
+++ download.py
@@ -15,6 +15,8 @@ import argparse
 import platform
 import logging
 import sys
+import time
+from time import strftime
 
 AUTO_DELETE = False
 
@@ -22,15 +24,22 @@ AUTO_DELETE = False
 stv_show_path = ""
 stv_sync_list = {}
 stv_skip_list = {}
-stv_args_list = ['--config','--store','--autodelete','--interactive','--logfile','--loglevel']
+stv_args_list = ['--config','--store','--autodelete','--interactive','--logfile','--loglevel','--quality']
 stv_illegal_chars_list_cmn = [':']
 stv_illegal_chars_list_win = ['|','/','?','<','>','*','\\','"']
+stv_quality = ""
 
 LOGLEVELS = {'debug': logging.DEBUG,
           'info': logging.INFO,
           'warning': logging.WARNING,
           'error': logging.ERROR,
           'critical': logging.CRITICAL}
+
+QUALITYLEVELS = {'auto':'0',
+                 'full':'4500000',
+                 'high':'2000000',
+                 'medium':'1300000',
+                 'low':'800000'}
 
 def select_show(stv):
     shows = simple.get_shows()
@@ -39,7 +48,7 @@ def select_show(stv):
         if int(show['recordings']) != 0:
             print str(val) + ": " + show['name'].encode('utf-8') + " [" + show['recordings'] + " episodes]"
             logging.info(str(val) + ": " + show['name'].encode('utf-8') + " [" + show['recordings'] + " episodes]")
-    show_id = ''#raw_input("Select show (#): ")
+    show_id = raw_input("Select show (#): ")
     print "-" * 25
     if show_id.lower() == 'a' or show_id == '':
         print "Download all shows, all episodes"
@@ -61,10 +70,10 @@ def select_episode(show):
         print "Downloading All files.."
         for x in range(len(episodes)):
             episode = episodes[x]
-            download_episode(show, episode)
+            download_episode(show, episode, QUALITYLEVELS.get(stv_quality,'4500000'))
     else:
         episode = episodes[int(episode_id)]
-        download_episode(show, episode)
+        download_episode(show, episode, QUALITYLEVELS.get(stv_quality,'4500000'))
 
 
 def generate_filename_menu(episodes, show):
@@ -130,15 +139,15 @@ def sanitize_filename(filename):
     logging.debug("Cleaning " + filename + " to " + clean_filename)
     return clean_filename
     
-def download_episode(show, episode):
+def download_episode(show, episode, quality):
     global stv_show_path
     instance_id = episode['instance_id']
     item_id = episode['item_id']
-    quality = 0
     group_id = show['group_id']
     file_name = stv_show_path + episode['filename'] + '.mp4'
 
     if not os.path.exists(file_name):
+
       url = simple.retrieve_episode_mp4(group_id, instance_id, item_id, quality)
       if not url:
           logging.error("Unable to retrieve URL for " \
@@ -151,9 +160,11 @@ def download_episode(show, episode):
       print "Downloading Episode: " + file_name
       logging.info("Downloading Episode: " + file_name)
       (filename, headers) = urllib.urlretrieve(url, file_name)
+
       file_size = os.path.getsize(file_name) >> 20
       print "File size: ", file_size , "MB"
       download_complete_bool = True if file_size > 20 else False
+
       print "download_complete_bool: " , download_complete_bool
       if AUTO_DELETE and download_complete_bool:
           url = "https://stv-p-api1-prod.rsslabs.net/content/actionjson/mediaserver/"
@@ -177,6 +188,31 @@ def is_show_skipped(show, stv):
                 return True
     return False
 
+def get_epsiode_quality(urls, prev_quality):
+    # Extract the Quality numbers from the urls
+    qual_levels = []
+    for n in range(0, len(urls)):
+        qual_levels.append(urls[n][3:-4])
+        logging.debug("Adding Quality Level: " + urls[n][3:-4])
+    qual_levels.sort(key=int,reverse=True)
+
+    if QUALITYLEVELS.get(stv_quality,'0') == '0':
+        #Auto is being used - going to start at highest quality, if no available
+        if int(prev_quality) == 0:
+            logging.debug("No previous quality - selecting " + qual_levels[0])
+            return qual_levels[0]
+        
+        #If there was a previous quality used, we should try and go lower
+        for lvl in range(0,len(qual_levels)):
+            if int(qual_levels[lvl]) < int(prev_quality):
+                return qual_levels[lvl]
+    else:
+        #Quality was requested - do we have it?
+        for lvl in range(0,len(qual_levels)):
+            if qual_levels[lvl] == QUALITYLEVELS.get(stv_quality,'0'):
+                return qual_levels[lvl]
+    return '0'
+
 def download_all_shows(shows,stv):
     for val, show in enumerate(shows):
         show = shows[val]
@@ -191,13 +227,31 @@ def download_all_shows(shows,stv):
             episodes = generate_filename_menu(episodes, show)
             for x in range(len(episodes)):
                 episode = episodes[x]
-                download_episode(show, episode)
+                s_info = simple._get_stream_urls(group_id, episode['instance_id'], episode['item_id'])
+                quality = get_epsiode_quality(s_info['urls'], '0')
+                if quality == '0':
+                    logging.error('Couldn\'t find the quality required: ' + stv_quality)
+                    return
+                else:
+                    logging.info("Selecting Quality: " + quality)
+                    download_episode(show, episode, quality)
+
+def auto_download_all(stv):
+    print "Auto Download all shows, all episodes"
+    shows = simple.get_shows()
+    for val, show in enumerate(shows):
+        # Only display shows with recordings (>0)
+        if int(show['recordings']) != 0:
+            logging.info(str(val) + ": " + show['name'].encode('utf-8') + " [" + show['recordings'] + " episodes]")
+    download_all_shows(shows,stv)
 
 def parse_config_file(args,config_file):
     global stv_show_path
     global stv_sync_list
     global stv_skip_list
     global logging
+    global stv_quality
+    
     config = ConfigParser.ConfigParser()
     if args.config: 
         config.read(args.config)
@@ -266,14 +320,14 @@ def parse_config_file(args,config_file):
                 # Need to setup stdout error handler
                 logging.basicConfig(stream=sys.stdout,
                                     level=LOGLEVELS.get(loglevel, logging.WARNING))
-
-    logging.info('Processing the following STVs...')
-    for n in stv_sync_list.keys():
-        logging.info(n)
-        logging.info("Skipping the following shows: \n" + stv_sync_list[n]['shows2skip'])
-    for n in stv_skip_list.keys():
-        logging.info("Skipping " + n)
-
+        
+        if (args.quality):
+            stv_quality = args.quality
+        else:
+            if sections['STVAPI']['quality']:
+                stv_quality = sections['STVAPI']['quality']
+        if stv_quality == "":
+            stv_quality = 'auto'
 
 if __name__ == "__main__":
     interactive = False;
@@ -284,16 +338,20 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
     if args.config:
         if os.path.exists(args.config): 
-            parse_config_file(args,"")
+            parse_config_file(args,None)
         else:
             print "Config file specified not found - " + args.config
-            interactive = True;
+            interactive = True
     else:
         if os.path.exists('stv-api.ini'):
             parse_config_file(args,'stv-api.ini')
         else:
             print "No config file found - reverting to interactive"
-            interactive = True;
+            interactive = True
+
+    # user can always override the settings.            
+    if args.interactive:
+        interactive = True
 
     if interactive:
         stv = raw_input("Enter STV Name: ")
@@ -302,13 +360,38 @@ if __name__ == "__main__":
         stv_sync_list[stv]['pass'] = getpass.getpass("Enter password: ")
         stv_sync_list[stv]['shows2skip'] = "" 
         stv_show_path = raw_input("Enter path to store shows: ")
+        stv_quality = raw_input("Which quality to get auto, full, high, medium or low: ")
         #ensure we have trailing path seperator
         if not stv_show_path.endswith(os.sep):
             stv_show_path+=os.sep
 
+        loglevel = 'warning'
+        if args.loglevel:
+            loglevel = args.loglevel
+        if args.logfile:
+            logfile = args.logfile
+            logging.basicConfig(filename=logfile,
+                                level=LOGLEVELS.get(loglevel, logging.WARNING))
+        else:
+            logging.basicConfig(stream=sys.stdout,
+                                level=LOGLEVELS.get(loglevel, logging.WARNING))
+    else:
+        logging.info('==================================================================')
+        logging.info(' STV API Download Script - Downloading Today: ' + strftime("%Y-%m-%d %H:%M:%S"))
+        logging.info('==================================================================')
+        logging.info('Processing the following STVs...')
+        for n in stv_sync_list.keys():
+            logging.info(n)
+            logging.info("Skipping the following shows: \n" + stv_sync_list[n]['shows2skip'])
+        for n in stv_skip_list.keys():
+            logging.info("Skipping " + n)
+        logging.info("Quality set to: " + stv_quality)
+
     for stv in stv_sync_list.keys():
         logging.info("Logging in to " + stv + "....")
         simple = api.SimpleTV(stv_sync_list[stv]['user'],stv_sync_list[stv]['pass'],stv)
-        select_show(stv);
+        if interactive:
+            select_show(stv);
+        else:
+            auto_download_all(stv);
         del simple
-
